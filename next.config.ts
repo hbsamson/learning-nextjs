32220e272df8f04dd053ba2b4ada677aad3146fe
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-05-07 04:16:31 +0800

    chapter 10 debounce search

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000, // check for changes every second
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
